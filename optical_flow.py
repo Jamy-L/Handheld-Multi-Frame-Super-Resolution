b0a63fe77368d383289115d7edb30107a6879eae
Jamy <jamylafenetre@gmail.com>
2022-09-15 18:42:25 +0200

    Updated LK + better evaluation
    LK compared with FarneBack

--- optical_flow.py
+++ optical_flow.py
@@ -65,22 +65,22 @@ def lucas_kanade_optical_flow(ref_img_bayer, comp_img_bayer, pre_alignment_bayer
     # grey level
     ref_img = (ref_img_bayer[::2, ::2] + ref_img_bayer[1::2, 1::2] + ref_img_bayer[::2, 1::2] + ref_img_bayer[1::2, ::2])/4
     comp_img = (comp_img_bayer[:,::2, ::2] + comp_img_bayer[:,1::2, 1::2] + comp_img_bayer[:,::2, 1::2] + comp_img_bayer[:,1::2, ::2])/4
-    pre_alignment = pre_alignment_bayer/2 # dividing by 2 because grey image is decimated by 2
+    pre_alignment = pre_alignment_bayer/2 # dividing by 2 because grey image twice smaller
 
     if verbose:
         current_time = getTime(
-            current_time, ' -- Grey Image decimated')
+            current_time, ' -- Grey Image estimated')
 
         
-    gradx = np.empty_like(ref_img)
-    grady = np.empty_like(ref_img)
+    gradsx = np.empty_like(comp_img)
+    gradsy = np.empty_like(comp_img)
     
     # Estimating gradients with cv2 sobel filters
     # Data format is very important. default 8uint is bad, because we use negative
-    # Values, and because may go up to a value of 3080. We need signed int16
-        
-    gradx = cv2.Sobel(ref_img, cv2.CV_16S, dx=1, dy=0)
-    grady = cv2.Sobel(ref_img, cv2.CV_16S, dx=0, dy=1)
+    # Values, and because may go higher. We need signed int16
+    for i in range(n_images):
+        gradsx[i] = cv2.Sobel(comp_img[i], cv2.CV_16S, dx=1, dy=0)
+        gradsy[i] = cv2.Sobel(comp_img[i], cv2.CV_16S, dx=0, dy=1)
 
     if verbose:
         current_time = getTime(
@@ -89,15 +89,15 @@ def lucas_kanade_optical_flow(ref_img_bayer, comp_img_bayer, pre_alignment_bayer
     alignment = cuda.to_device(pre_alignment) # init of flow, directly on GPU
     cuda_ref_img = cuda.to_device(np.ascontiguousarray(ref_img))
     cuda_comp_img = cuda.to_device(np.ascontiguousarray(comp_img))
-    cuda_gradx = cuda.to_device(gradx)
-    cuda_grady = cuda.to_device(grady)
+    cuda_gradsx = cuda.to_device(gradsx)
+    cuda_gradsy = cuda.to_device(gradsy)
     
     current_time = getTime(
         current_time, ' -- Arrays moved to GPU')
     
     for iter_index in range(n_iter):
         lucas_kanade_optical_flow_iteration(
-            cuda_ref_img, cuda_gradx, cuda_grady, cuda_comp_img, alignment,
+            cuda_ref_img, cuda_gradsx, cuda_gradsy, cuda_comp_img, alignment,
             options, params, iter_index)
         if debug :
             debug_list.append(alignment.copy_to_host())
@@ -110,7 +110,7 @@ def lucas_kanade_optical_flow(ref_img_bayer, comp_img_bayer, pre_alignment_bayer
     return alignment
 
 
-def lucas_kanade_optical_flow_iteration(ref_img, gradx, grady, comp_img, alignment, options, params, iter_index):
+def lucas_kanade_optical_flow_iteration(ref_img, gradsx, gradsy, comp_img, alignment, options, params, iter_index):
     """
     Computes one iteration of the Lucas-Kanade optical flow
 
@@ -154,7 +154,7 @@ def lucas_kanade_optical_flow_iteration(ref_img, gradx, grady, comp_img, alignme
 
         
     @cuda.jit
-    def get_new_flow(ref_img, comp_img, gradx, grady, alignment, last_it):
+    def get_new_flow(ref_img, comp_img, gradsx, gradsy, alignment, last_it):
         
         
         image_index, patch_idy, patch_idx = cuda.blockIdx.x, cuda.blockIdx.y, cuda.blockIdx.z
@@ -178,19 +178,23 @@ def lucas_kanade_optical_flow_iteration(ref_img, gradx, grady, comp_img, alignme
         inbound = inbound and  (0 <= pixel_global_idx < imsize_x) and (0 <= pixel_global_idy < imsize_y)
     
         if inbound :
+            # Warp I with W(x; p) to compute I(W(x; p))
             new_idx = round(pixel_global_idx + alignment[image_index, patch_idy, patch_idx, 0])
             new_idy = round(pixel_global_idy + alignment[image_index, patch_idy, patch_idx, 1])
         
         inbound = inbound and (0 <= new_idx < imsize_x) and (0 <= new_idy < imsize_y)
         
         if inbound:
-            gradx = gradx[new_idy, new_idx]
-            grady = grady[new_idy, new_idx]
-            gradt = int16(comp_img[image_index, pixel_global_idy, pixel_global_idx]) - int16(ref_img[new_idy, new_idx])
+            # Warp the gradient of I with W(x; p)
+            gradx = gradsx[image_index, new_idy, new_idx]
+            grady = gradsy[image_index, new_idy, new_idx]
+            # Images are of unsigned type for speeding up transfers. We need to sign them before substracting
+            gradt = int16(comp_img[image_index, new_idy, new_idx]) - int16(ref_img[pixel_global_idy, pixel_global_idx])
             
-            cuda.atomic.add(ATB, 0, gradx*gradt)
-            cuda.atomic.add(ATB, 1, grady*gradt)
+            cuda.atomic.add(ATB, 0, -gradx*gradt)
+            cuda.atomic.add(ATB, 1, -grady*gradt)
             
+            # Compute the Hessian matrix
             cuda.atomic.add(ATA, (0, 0), gradx**2)
             cuda.atomic.add(ATA, (1, 0), gradx*grady)
             cuda.atomic.add(ATA, (0, 1), gradx*grady)
@@ -199,8 +203,8 @@ def lucas_kanade_optical_flow_iteration(ref_img, gradx, grady, comp_img, alignme
         # We need the entire tile accumulation
         tile_disp = cuda.shared.array(2, dtype = DEFAULT_CUDA_FLOAT_TYPE)
         cuda.syncthreads()
-        if pixel_local_idx == 0 and pixel_local_idy == 0:
-            if ATA[0, 0]*ATA[1, 1] - ATA[0, 1]*ATA[1, 0] < EPSILON : # Aray cannot be inverted
+        if pixel_local_idx == 0 and pixel_local_idy == 0: # single threaded section
+            if ATA[0, 0]*ATA[1, 1] - ATA[0, 1]*ATA[1, 0] < EPSILON : # Array cannot be inverted
                 tile_disp[0] = 0
                 tile_disp[1] = 0
             else:
@@ -215,7 +219,7 @@ def lucas_kanade_optical_flow_iteration(ref_img, gradx, grady, comp_img, alignme
     current_time = time()
     
     get_new_flow[[(n_images, n_patch_y, n_patch_x), (tile_size, tile_size)]
-        ](ref_img, comp_img, gradx, grady, alignment, (n_iter - 1) == iter_index)
+        ](ref_img, comp_img, gradsx, gradsy, alignment, (n_iter - 1) == iter_index)
     
     if verbose:
         current_time = getTime(
@@ -259,11 +263,33 @@ def get_closest_flow(idx_sub, idy_sub, optical_flows, tile_size, imsize, local_f
     
     # Side condition when the required patch is not existing (because it would be 
     # too small for block matching)
-    elif patch_idy_top >= imshape[0] or patch_idx_left >= imshape[1]:
-        flow_x = 0
-        flow_y = 0
+    elif patch_idy_top >= imshape[0] and patch_idx_left >= imshape[1]:
+        flow_x = optical_flows[-1, -1, 0]
+        flow_y = optical_flows[-1, -1, 1]
+        
+    elif patch_idy_top >= imshape[0]:
+        if patch_idx_left >= 0 and patch_idx_right <imshape[1]:
+            flow_x = (optical_flows[-1, patch_idx_left, 0] + optical_flows[-1, patch_idx_right, 0])/2
+            flow_y = (optical_flows[-1, patch_idx_left, 1] + optical_flows[-1, patch_idx_right, 1])/2
+        elif patch_idx_left >= 0 :
+            flow_x = optical_flows[-1, patch_idx_left, 0]
+            flow_y = optical_flows[-1, patch_idx_left, 1]
+        elif patch_idx_right < imshape[1] :
+            flow_x = optical_flows[-1, patch_idx_right, 0]
+            flow_y = optical_flows[-1, patch_idx_right, 1]
 
-
+    elif patch_idx_left >= imshape[1]:
+        if patch_idy_top >= 0 and patch_idy_bottom < imshape[0]:
+            flow_x = (optical_flows[patch_idy_top, -1, 0] + optical_flows[patch_idy_bottom, -1, 0])/2
+            flow_y = (optical_flows[patch_idy_top, -1, 1] + optical_flows[patch_idy_bottom, -1, 1])/2
+        elif patch_idy_top >= 0 :
+            flow_x = optical_flows[patch_idy_top, -1, 0]
+            flow_y = optical_flows[patch_idy_top, -1, 1]
+        elif patch_idy_bottom < imshape[1] :
+            flow_x = optical_flows[patch_idy_bottom, -1, 0]
+            flow_y = optical_flows[patch_idy_bottom, -1, 1]
+            
+            
     # corner conditions
     elif patch_idy_bottom >= imshape[0] and patch_idx_left < 0:
         flow_x = optical_flows[patch_idy_top, patch_idx_right, 0]
