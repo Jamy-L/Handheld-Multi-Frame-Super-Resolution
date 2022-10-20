64b6a3e2ffcc1ee5f439656d885a4d08d078bd95
Jamy <jamylafenetre@gmail.com>
2022-10-20 17:41:21 +0200

    Plot merge for new merge

--- handheld_super_resolution/raw2rgb.py
+++ handheld_super_resolution/raw2rgb.py
@@ -209,7 +209,8 @@ def postprocess(raw, img=None, do_color_correction=True, do_tonemapping=False,
         ## Sharpening
         if do_sharpening:
             ## TODO: polyblur instead
-            img = filters.unsharp_mask(img)
+            img = filters.unsharp_mask(img, radius=3, amount=2,
+                                       channel_axis=2, preserve_range=True)
         ## Tone mapping
         if do_tonemapping:
             img = apply_smoothstep(img)
