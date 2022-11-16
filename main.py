7162a4d2d50198c59d5fee4be458697cf267d4ba
Jamy <jamylafenetre@gmail.com>
2022-11-16 15:34:08 +0100

    update D_tr

--- main.py
+++ main.py
@@ -47,10 +47,10 @@ def cfa_to_grayscale(raw_img):
 # logger.setLevel(logging.WARNING)
 
 # crop_str = "[1638:2600, 1912:2938]" # for friant
-# crop_str = "[1002:1686, 2406:3130]" # for rue4 (arrondissement)
+crop_str = "[1002:1686, 2406:3130]" # for rue4 (arrondissement)
 # crop_str = "[1002:1686, 2000:2700]" # for rue4 (truck plate)
 # crop_str = "[500:2500, 1000:2500]" # for samsung 0
-crop_str=None
+# crop_str=None
 # crop_str = "[1500:2500, 2000:3000]" # for samsung 1
 
 #%%
@@ -59,11 +59,11 @@ params = get_params(PSNR = 35)
 params["scale"] = 1.5
 options = {'verbose' : 3}
 
-params['merging']['kernel'] = 'act'
+params['merging']['kernel'] = 'handheld'
 params['robustness']['on'] = False
 params['kanade']['tuning']['kanadeIter'] = 3
-burst_path = 'P:/inriadataset/inriadataset/pixel4a/friant/raw/'
-# burst_path = 'P:/inriadataset/inriadataset/pixel3a/rue4/raw'
+# burst_path = 'P:/inriadataset/inriadataset/pixel4a/friant/raw/'
+burst_path = 'P:/inriadataset/inriadataset/pixel3a/rue4/raw'
 # burst_path = 'P:/0001/Samsung'
 
 output, R, r, alignment, covs = process(burst_path, options, params, crop_str)
