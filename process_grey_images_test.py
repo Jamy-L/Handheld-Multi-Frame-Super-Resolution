3fa0d9b9efb278072880a60d28a75c25ad73e48e
Jamy <jamylafenetre@gmail.com>
2022-11-16 15:34:08 +0100

    update D_tr

--- process_grey_images_test.py
+++ process_grey_images_test.py
@@ -38,7 +38,7 @@ params = {'mode':'grey',
                 'tuning': {
                     # WARNING: these parameters are defined fine-to-coarse!
                     'factors': [1, 2, 2, 2],
-                    'tileSizes': [16, 16, 16, 8],
+                    'tileSizes': [32, 16, 16, 8],
                     'searchRadia': [1, 4, 4, 4],
                     'distances': ['L1', 'L2', 'L2', 'L2'],
                     # if you want to compute subpixel tile alignment at each pyramid level
@@ -50,7 +50,7 @@ params = {'mode':'grey',
                 'epsilon div' : 1e-6,
                 'grey method':'FFT',
                 'tuning' : {
-                    'tileSize' : 16,
+                    'tileSize' : 32,
                     'kanadeIter': 6, # 3 
                     'sigma blur':0,
                     }},
@@ -59,7 +59,7 @@ params = {'mode':'grey',
                 'exif':{'CFA Pattern':CFA},
                 'mode':'gray',
                 'tuning' : {
-                    'tileSize': 16,
+                    'tileSize': 32,
                     't' : 0.12,            # 0.12
                     's1' : 12,          #12
                     's2' : 2,           # 2
@@ -72,11 +72,11 @@ params = {'mode':'grey',
                 'scale': 2,
                 'kernel' : 'handheld',
                 'tuning': {
-                    'tileSize': 16,
+                    'tileSize': 32,
                     'k_detail' : 0.33, # [0.25, ..., 0.33]
                     'k_denoise': 5,    # [3.0, ...,5.0]
-                    'D_th': 0.05,      # [0.001, ..., 0.010]
-                    'D_tr': 0.014,     # [0.006, ..., 0.020]
+                    'D_th': 0.01,      # [0.001, ..., 0.010]
+                    'D_tr': 0.02*16,     # [0.006, ..., 0.020]
                     'k_stretch' : 4,   # 4
                     'k_shrink' : 2,    # 2
                     }
@@ -86,11 +86,63 @@ params['robustness']['diff_curve'] = np.load('C:/Users/jamyl/Documents/GitHub/Ha
 options = {'verbose' : 0}
 
 #%%
-burst_path = 'P:/results_surecavi_20220325/results_anger/20220325/data/exp2-stack'
-output_path = 'P:/results_surecavi_20220325/results_anger/20220325/results_handheld/exp2'
+burst_path = 'P:/exp_noise/exp7/ref'
+raw_path_list = glob.glob(os.path.join(burst_path, '*.tif'))
+
+
+index_start = 0
+burst_size=15
+first_image_path = raw_path_list[index_start]
+
+raw_ref_img = cv2.imread(first_image_path, -1)
+
+comp_images = cv2.imread(raw_path_list[index_start+1], -1)[None]
+for i in range(index_start+2,index_start+burst_size+1):
+    comp_images = np.append(comp_images, cv2.imread(raw_path_list[i], -1)[None], axis=0)
+
+raw_ref_img = raw_ref_img/(2**16 - 1)
+comp_images = comp_images/(2**16 - 1)
+
+output, R, r, alignment, covs = main(np.ascontiguousarray(raw_ref_img).astype(np.float32),
+                                     np.ascontiguousarray(comp_images).astype(np.float32),
+                                     options, params)
+
+
+filtered = filters.unsharp_mask(output[:,:,0], radius=3, amount=1.5,
+                           channel_axis=None, preserve_range=True)
+plt.figure('output')
+plt.imshow(output[:,:,0], cmap="gray")
+#%%
+
+
+
+im_id = 0
+D = covs[:, :, :, 0, 0]
+A = covs[:, :, :, 0, 1]
+l1 = covs[:, :, :, 1, 0]
+
+
+
+
+plt.figure("A {}".format(im_id))
+plt.imshow(A[im_id], cmap="gray", vmin=1, vmax = 2)
+plt.colorbar()
+
+plt.figure("L1 {}".format(im_id))
+plt.imshow(l1[im_id], cmap="gray", vmin=0, vmax = 0.05)
+plt.colorbar()
+
+plt.figure("D {} (bayer grad)".format(im_id))
+plt.imshow(D[im_id], cmap="gray", vmin=0, vmax = 1)
+plt.colorbar()
+
+
+#%%
+burst_path = 'P:/exp_noise/exp6/ref'
+output_path = 'P:/exp_noise/handheld_output/exp6/Low SNR'
 
 def process_burst(ref_id, burst_size):
-    raw_path_list = glob.glob(os.path.join(burst_path, '*.png'))
+    raw_path_list = glob.glob(os.path.join(burst_path, '*.tif'))
 
 
     index_start = ref_id
@@ -108,8 +160,13 @@ def process_burst(ref_id, burst_size):
     output, R, r, alignment, covs = main(np.ascontiguousarray(raw_ref_img).astype(np.float32),
                                          np.ascontiguousarray(comp_images).astype(np.float32),
                                          options, params)
+    filtered = filters.unsharp_mask(output[:,:,0], radius=3, amount=1.5,
+                               channel_axis=None, preserve_range=True)
+    filtered*=2**16-1
+    filtered=np.clip(filtered, 0, 2**16-1)
+    filtered = filtered.astype(np.uint16)
     name = "{}/{}-N{}.tiff".format(output_path, ref_id, burst_size)
-    cv2.imwrite(name,filters.unsharp_mask(output[:,:,0], radius=3, amount=1.5,
-                               channel_axis=None, preserve_range=True))
-for i in tqdm(range(40, 89)):
-    process_burst(i, 11)
+    
+    cv2.imwrite(name, filtered)
+for i in tqdm(range(0, 84)):
+    process_burst(i, 15)
