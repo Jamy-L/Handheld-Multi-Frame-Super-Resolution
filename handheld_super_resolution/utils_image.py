a9798c05119ffd44bfbea0be58ef9dc08cce36af
Jamy L <46826148+Jamy-L@users.noreply.github.com>
2023-01-16 19:15:53 +0100

    Update utils_image.py

--- handheld_super_resolution/utils_image.py
+++ handheld_super_resolution/utils_image.py
@@ -1,31 +1,3 @@
-
-""" Utility functions that work on images / arrays of images.
-Copyright (c) 2021 Antoine Monod
-
-This program is free software: you can redistribute it and/or modify it
-under the terms of the GNU Affero General Public License
-as published by the Free Software Foundation, either version 3 of the License,
-or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-See the GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License along with this program.
-If not, see <http://www.gnu.org/licenses/>.
-
-This file implements an algorithm possibly linked to the patent US9077913B2.
-This file is made available for the exclusive aim of serving as scientific tool
-to verify the soundness and completeness of the algorithm description.
-Compilation, execution and redistribution of this file may violate patents rights in certain countries.
-The situation being different for every country and changing over time,
-it is your responsibility to determine which patent rights restrictions apply to you
-before you compile, use, modify, or redistribute this file.
-A patent lawyer is qualified to make this determination.
-If and only if they don't conflict with any patent terms,
-you can benefit from the following license terms attached to this file.
-"""
-
 import math
 
 import numpy as np
