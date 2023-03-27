a20bec5ebe135f662e1b40aa343a2e011fb5f57d
Jamy <jamylafenetre@gmail.com>
2023-03-27 14:09:00 +0200

    Cleaned repo

--- handheld_super_resolution/fast_monte_carlo.py
+++ handheld_super_resolution/fast_monte_carlo.py
@@ -2,12 +2,16 @@
 """
 Created on Tue Mar 21 11:49:06 2023
 
+This script is an extension of monte_carlo_simulation, optimised to run faster.
+The simulation uses multiple CPU cores and focuses on the non linear parts of
+the curve. Linear parts are interpolated, resulting in a considerable time gain.
+
+
 @author: jamyl
 """
 
 import numpy as np
 from tqdm import tqdm
-import os
 import matplotlib.pyplot as plt
 import multiprocessing
 from functools import partial
@@ -18,8 +22,10 @@ n_brightness_levels = 1000
 
 # alpha = 1.80710882e-4
 # beta = 3.1937599182128e-6
-tol = 3
-# While I +- tol*sigma is not saturated, the linear model is considered to hold.
+TOL = 3
+
+# While I +- tol*sigma is between 0 and 1, the linear model is considered
+# to hold, and values are interpolated.
 # 3 and 5 are good values  
 
 #%%
@@ -35,6 +41,27 @@ def get_non_linearity_bound(alpha, beta, tol):
 
 
 def unitary_MC(alpha, beta, b):
+    """
+    Runs a MC scheme to estimate sigma and d for a given brightness, alpha and
+    beta.
+
+    Parameters
+    ----------
+    alpha : TYPE
+        DESCRIPTION.
+    beta : TYPE
+        DESCRIPTION.
+    b : float in 0, 1
+        brighntess
+
+    Returns
+    -------
+    diff_mean : float
+        mean difference
+    std_mean : float
+        mean standard deviation
+
+    """
     # create the patch
     patch = np.ones((n_patches, 3, 3)) * b
 
@@ -45,7 +72,7 @@ def unitary_MC(alpha, beta, b):
     patch2 = patch + np.sqrt(patch * alpha + beta) * np.random.randn(*patch.shape)
     patch2 = np.clip(patch2, 0.0, 1.0)
  
-    # compute statistics and store
+    # compute statistics
     std_mean = 0.5 * np.mean((np.std(patch1, axis=(1, 2)) + np.std(patch2, axis=(1, 2))))
 
     curr_mean1 = np.mean(patch1, axis=(1, 2))
@@ -55,6 +82,26 @@ def unitary_MC(alpha, beta, b):
     return diff_mean, std_mean
 
 def regular_MC(b_array, alpha, beta):
+    """
+    Runs MC on multiple CPU cores
+
+    Parameters
+    ----------
+    b_array : numpy array [n_brightness_levels]
+        required brighntess levels.
+    alpha : TYPE
+        DESCRIPTION.
+    beta : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    sigmas : numpy array [n_brightness_levels]
+        Simulated std
+    diffs : numpy array [n_brightness_levels]
+        simulated differences
+
+    """
     if multiprocessing.cpu_count() > 1:
         N_CPU = multiprocessing.cpu_count()-1
     else:
@@ -70,12 +117,35 @@ def regular_MC(b_array, alpha, beta):
         diffs[b_] = result[0]
         sigmas[b_] = result[1]
     
-    
+    pool.close()
     return sigmas, diffs
 
 def interp_MC(b_array,
               sigma_min, sigma_max,
               diff_min, diff_max):
+    """
+    Interpolates the missing values for diff and and sigma, based on their
+    upper and lower bounds.
+
+    Parameters
+    ----------
+    b_array : TYPE
+        DESCRIPTION.
+    sigma_min : TYPE
+        DESCRIPTION.
+    sigma_max : TYPE
+        DESCRIPTION.
+    diff_min : TYPE
+        DESCRIPTION.
+    diff_max : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+
+    """
     norm_b = (b_array - b_array[0])/(b_array[-1] - b_array[0])
     
     sigmas_sq_lin = norm_b * (sigma_max**2 - sigma_min**2) + sigma_min**2
@@ -84,12 +154,32 @@ def interp_MC(b_array,
     return np.sqrt(sigmas_sq_lin[1:-1]), np.sqrt(diffs_sq_lin[1:-1])
 
 
-def run_fast_MC(alpha, beta): 
-    t0 = time.perf_counter()
-    print("Beginning...")
-    xmin, xmax = get_non_linearity_bound(alpha, beta, tol)
+def run_fast_MC(alpha, beta):
+    """
+    Computes diff and sigmas for 1001 values of brighntess, for fixed alpha and
+    beta.
     
-    # these 2 inddexes define the points for which the linear model is valid.
+    To go faster, bound for which the probability of clipping is
+    neglectible are estimated (xmin, xmax). A regular MC is applied to brightness
+    outside of this range, and the rest of the points are interpolates linearly.
+
+    Parameters
+    ----------
+    alpha : TYPE
+        DESCRIPTION.
+    beta : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    # t0 = time.perf_counter()
+    print("Estimating noise curves ...")
+    xmin, xmax = get_non_linearity_bound(alpha, beta, TOL)
+    
+    # these 2 indexes define the points for which the linear model is valid.
     # The regular Monte carlo is applied to these points, that are then used as reference
     # for the linear model
     imin = int(np.ceil(xmin * n_brightness_levels)) + 1
@@ -114,26 +204,27 @@ def run_fast_MC(alpha, beta):
                                  diffs[imin], diffs[imax])
     sigmas[imin:imax+1] = sigmas_l
     diffs[imin:imax+1] = diffs_l
-    print('Finished! Estimated 1 noise curve in {:2f} sec'.format(time.perf_counter() - t0))
+    # print('Finished! Estimated 1 noise curve in {:2f} sec'.format(time.perf_counter() - t0))
 
 
 
     
-    plt.figure("sigma")
-    plt.plot(np.linspace(0, 1, n_brightness_levels+1), sigmas)
-    plt.grid()
-    plt.axline((xmin, 0), (xmin, 1e-5), c='k')
-    plt.axline((xmax, 0), (xmax, 1e-5), c='k')
-    plt.ylabel('Sigma')
-    plt.xlabel('brightness')
+    # plt.figure("sigma")
+    # plt.plot(np.linspace(0, 1, n_brightness_levels+1), sigmas)
+    # plt.grid()
+    # plt.axline((xmin, 0), (xmin, 1e-5), c='k')
+    # plt.axline((xmax, 0), (xmax, 1e-5), c='k')
+    # plt.ylabel('Sigma')
+    # plt.xlabel('brightness')
     
-    plt.figure("Diff")
-    plt.plot(np.linspace(0, 1, n_brightness_levels+1), diffs)
-    plt.grid()
-    plt.axline((xmin, 0), (xmin, 1e-5), c='k')
-    plt.axline((xmax, 0), (xmax, 1e-5), c='k')
-    plt.ylabel('Diff')
-    plt.xlabel('brightness')
+    # plt.figure("Diff")
+    # plt.plot(np.linspace(0, 1, n_brightness_levels+1), diffs)
+    # plt.grid()
+    # plt.axline((xmin, 0), (xmin, 1e-5), c='k')
+    # plt.axline((xmax, 0), (xmax, 1e-5), c='k')
+    # plt.ylabel('Diff')
+    # plt.xlabel('brightness')
+    return sigmas, diffs
     
 
 if __name__ == "__main__":
--- run_handheld.py
+++ run_handheld.py
@@ -6,175 +6,190 @@ Created on Thu Feb 23 13:51:42 2023
 """
 
 import os
+import glob
+
+import argparse
+from pathlib import Path
 import numpy as np
 import torch
-import argparse
 from skimage import img_as_float32, img_as_ubyte, io, color, filters
 import cv2
 import rawpy
 
 from handheld_super_resolution import process
+from handheld_super_resolution.utils_dng import save_as_dng 
 
 
-
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
-#### Argparser
-
-def str2bool(v):
-    v = str(v)
-
-    if isinstance(v, bool):
-        return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
+if __name__ == "__main__":
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    
+    #### Argparser
+    
+    def str2bool(v):
+        v = str(v)
+    
+        if isinstance(v, bool):
+            return v
+        if v.lower() in ('yes', 'true', 't', 'y', '1'):
+            return True
+        elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+            return False
+        else:
+            raise argparse.ArgumentTypeError('Boolean value expected.')
+    
+    parser = argparse.ArgumentParser()
+    ## Image parameter
+    parser.add_argument('--impath', type=str, help='input image')
+    parser.add_argument('--outpath', type=str, help='out image')
+    parser.add_argument('--scale', type=int, default=2, help='Scaling factor')
+    parser.add_argument('--verbose', type=int, default=1, help='Verbose option (0 to 4)')
+    
+    ## Robustness
+    parser.add_argument('--t', type=float,  default=0.12, help='Threshold for robustness')
+    parser.add_argument('--s1', type=float,  default=2, help='Threshold for robustness')
+    parser.add_argument('--s2', type=float,  default=12, help='Threshold for robustness')
+    parser.add_argument('--Mt', type=float,  default=0.8, help='Threshold for robustness')
+    parser.add_argument('--R_on', type=str2bool,  default=True, help='Whether robustness is activated or not')
+    
+    parser.add_argument('--R_denoising_on', type=str2bool, default=True, help='Whether or not the robustness based denoising should be applied')
+    
+    
+    ## Post Processing
+    parser.add_argument('--post_process', type=str2bool,  default=True, help='Whether post processing should be applied or not')
+    parser.add_argument('--do_sharpening', type=str2bool,  default=True, help='Whether sharpening should be applied during post processing')
+    parser.add_argument('--radius', type=float,  default=3, help='If sharpening is applied, radius of the unsharp mask')
+    parser.add_argument('--amount', type=float,  default=1.5, help='If sharpening is applied, amount of the unsharp mask')
+    parser.add_argument('--do_tonemapping', type=str2bool,  default=False, help='Whether tonnemaping should be applied during post processing')
+    parser.add_argument('--do_gamma', type=str2bool,  default=False, help='Whether gamma curve should be applied during post processing')
+    parser.add_argument('--do_color_correction', type=str2bool,  default=False, help='Whether color correction should be applied during post processing')
+    
+    
+    ## Merging (advanced)
+    parser.add_argument('--kernel_shape', type=str, default='handheld', help='"handheld" or "iso" : Whether to use steerable or isotropic kernels')
+    parser.add_argument('--k_detail', type=float, default=None, help='SNR based by default')
+    parser.add_argument('--k_denoise', type=float, default=None, help='SNR based by default')
+    parser.add_argument('--k_stretch', type=float, default=4)
+    parser.add_argument('--k_shrink', type=float, default=2)
+    
+    ## Alignment (advanced)
+    parser.add_argument('--ICA_iter', type=int, default=3, help='Number of ICA Iterations')
+    
+    
+    args = parser.parse_args()
+    
+    
+    
+    print('Parameters:')
+    print('')
+    print('  Upscaling factor:       %d' % args.scale)
+    print('')
+    if args.scale == 1:
+        print('    Demosaicking mode')
     else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
-
-parser = argparse.ArgumentParser()
-## Image parameter
-parser.add_argument('--impath', type=str, help='input image')
-parser.add_argument('--outpath', type=str, help='out image')
-parser.add_argument('--scale', type=int, default=2, help='Scaling factor')
-parser.add_argument('--verbose', type=int, default=1, help='Verbose option (0 to 4)')
-
-## Robustness
-parser.add_argument('--t', type=float,  default=0.12, help='Threshold for robustness')
-parser.add_argument('--s1', type=float,  default=2, help='Threshold for robustness')
-parser.add_argument('--s2', type=float,  default=12, help='Threshold for robustness')
-parser.add_argument('--Mt', type=float,  default=0.8, help='Threshold for robustness')
-parser.add_argument('--R_on', type=str2bool,  default=True, help='Whether robustness is activated or not')
-
-parser.add_argument('--R_denoising_on', type=str2bool, default=True, help='Whether or not the robustness based denoising should be applied')
-
-
-## Post Processing
-parser.add_argument('--post_process', type=str2bool,  default=True, help='Whether post processing should be applied or not')
-parser.add_argument('--do_sharpening', type=str2bool,  default=True, help='Whether sharpening should be applied during post processing')
-parser.add_argument('--radius', type=float,  default=3, help='If sharpening is applied, radius of the unsharp mask')
-parser.add_argument('--amount', type=float,  default=1.5, help='If sharpening is applied, amount of the unsharp mask')
-parser.add_argument('--do_tonemapping', type=str2bool,  default=False, help='Whether tonnemaping should be applied during post processing')
-parser.add_argument('--do_gamma', type=str2bool,  default=False, help='Whether gamma curve should be applied during post processing')
-parser.add_argument('--do_color_correction', type=str2bool,  default=False, help='Whether color correction should be applied during post processing')
-
-
-## Merging (advanced)
-parser.add_argument('--kernel_shape', type=str, default='handheld', help='"handheld" or "iso" : Whether to use steerable or isotropic kernels')
-parser.add_argument('--k_detail', type=float, default=None, help='SNR based by default')
-parser.add_argument('--k_denoise', type=float, default=None, help='SNR based by default')
-parser.add_argument('--k_stretch', type=float, default=4)
-parser.add_argument('--k_shrink', type=float, default=2)
-
-## Alignment (advanced)
-parser.add_argument('--ICA_iter', type=int, default=3, help='Number of ICA Iterations')
-
-
-args = parser.parse_args()
-
-
-
-print('Parameters:')
-print('')
-print('  Upscaling factor:       %d' % args.scale)
-print('')
-if args.scale == 1:
-    print('    Demosaicking mode')
-else:
-    print('    Super-resolution mode.')
-    if args.scale > 2:
-        print('    WARNING: Since the optics and the integration on the sensor limit the aliasing, do not expect more details than that obtained at x2 (refer to our paper and the original publication).')
-print('')
-if args.R_on:
-    print('  Robustness:       enabled')
+        print('    Super-resolution mode.')
+        if args.scale > 2:
+            print('    WARNING: Since the optics and the integration on the sensor limit the aliasing, do not expect more details than that obtained at x2 (refer to our paper and the original publication).')
+    print('')
+    if args.R_on:
+        print('  Robustness:       enabled')
+        print('  -------------------------')
+        print('  t:                      %1.2f' % args.t)
+        print('  s1:                     %1.2f' % args.s1)
+        print('  s2:                     %1.2f' % args.s2)
+        print('  Mt:                     %1.2f' % args.Mt)
+        if args.R_denoising_on:
+            print('  Robustness denoising:   enabled')
+        else:
+            print('  Robustness denoising:   disabled')
+        print('                            ' )
+    else:
+        print('  Robustness:      disabled')
+        print('                            ' )
+    
+    print('  Alignment:')
     print('  -------------------------')
-    print('  t:                      %1.2f' % args.t)
-    print('  s1:                     %1.2f' % args.s1)
-    print('  s2:                     %1.2f' % args.s2)
-    print('  Mt:                     %1.2f' % args.Mt)
-    if args.R_denoising_on:
-        print('  Robustness denoising:   enabled')
+    print('  ICA Iterations:         %d' % args.ICA_iter)
+    print('')
+    print('  Fusion:')
+    print('  -------------------------')
+    print('  Kernel shape:           %s' % args.kernel_shape)
+    print('  k_stretch:              %1.2f' % args.k_stretch)
+    print('  k_shrink:               %1.2f' % args.k_shrink)
+    if args.k_detail is not None:
+        print('  k_detail:               %1.2f' % args.k_detail)
     else:
-        print('  Robustness denoising:   disabled')
-    print('                            ' )
-else:
-    print('  Robustness:      disabled')
-    print('                            ' )
-
-print('  Alignment:')
-print('  -------------------------')
-print('  ICA Iterations:         %d' % args.ICA_iter)
-print('')
-print('  Fusion:')
-print('  -------------------------')
-print('  Kernel shape:           %s' % args.kernel_shape)
-print('  k_stretch:              %1.2f' % args.k_stretch)
-print('  k_shrink:               %1.2f' % args.k_shrink)
-if args.k_detail is not None:
-    print('  k_detail:               %1.2f' % args.k_detail)
-else:
-    print('  k_detail:               SNR based' )
-if args.k_denoise is not None:
-    print('  k_denoise:              %1.2f' % args.k_denoise)
-else:
-    print('  k_denoise:              SNR based' )
-print('')
-
-
-
-
-
-#### Handheld ####
-print('Processing with handheld super-resolution')
-options = {'verbose' : args.verbose}
-params={"scale": args.scale,
-        "merging":{"kernel":args.kernel_shape},
-        "robustness":{"on":args.R_on},
-        "kanade": {"tuning": {"kanadeIter":args.ICA_iter}}
-        }
-
-
-params['robustness']['tuning'] = {'t' : args.t,
-                                  's1' : args.s1,
-                                  's2' : args.s2,        
-                                  'Mt' : args.Mt,       
-                                  }
-
-if args.k_detail is not None:
-    params['merging']['tuning']['k_detail'] = args.k_detail
-if args.k_denoise is not None:
-    params['merging']['tuning']['k_denoise'] = args.k_denoise
-
-params['merging'] = {'tuning' : {'k_stretch' : args.k_stretch,
-                                 'k_shrink' : args.k_shrink
-                                 }}
-params['accumulated robustness denoiser'] = {'on': args.R_denoising_on}
-
-params['post processing'] = {'on':args.post_process,
-                    'do sharpening' : args.do_sharpening,
-                    'do tonemapping':args.do_tonemapping,
-                    'do gamma' : args.do_gamma,
-                    'do devignette' : False,
-                    'do color correction': args.do_color_correction,
+        print('  k_detail:               SNR based' )
+    if args.k_denoise is not None:
+        print('  k_denoise:              %1.2f' % args.k_denoise)
+    else:
+        print('  k_denoise:              SNR based' )
+    print('')
+    
+    
+    
+    
+    
+    #### Handheld ####
+    print('Processing with handheld super-resolution')
+    options = {'verbose' : args.verbose}
+    params={"scale": args.scale,
+            "merging":{"kernel":args.kernel_shape},
+            "robustness":{"on":args.R_on},
+            "kanade": {"tuning": {"kanadeIter":args.ICA_iter}}
+            }
+    
+    
+    params['robustness']['tuning'] = {'t' : args.t,
+                                      's1' : args.s1,
+                                      's2' : args.s2,        
+                                      'Mt' : args.Mt,       
+                                      }
+    
+    if args.k_detail is not None:
+        params['merging']['tuning']['k_detail'] = args.k_detail
+    if args.k_denoise is not None:
+        params['merging']['tuning']['k_denoise'] = args.k_denoise
+    
+    params['merging'] = {'tuning' : {'k_stretch' : args.k_stretch,
+                                     'k_shrink' : args.k_shrink
+                                     }}
+    params['accumulated robustness denoiser'] = {'on': args.R_denoising_on}
+    
+    outpath = Path(args.outpath)
+    # disabling post processing for dng outputs
+    if outpath.suffix == '.dng':
+        args.post_process = False
+    
+    
+    params['post processing'] = {'on':args.post_process,
+                        'do sharpening' : args.do_sharpening,
+                        'do tonemapping':args.do_tonemapping,
+                        'do gamma' : args.do_gamma,
+                        'do devignette' : False,
+                        'do color correction': args.do_color_correction,
+            
+                        'sharpening' : {'radius': args.radius,
+                                        'ammount': args.amount}
+                        }
+    
+    
+    handheld_output = process(args.impath, options, params)
+    handheld_output = np.nan_to_num(handheld_output)
+    handheld_output = np.clip(handheld_output, 0, 1)
+    
+    
+    # define a faster imsave for large png images
+    def imsave(fname, rgb_8bit_data):
+        return cv2.imwrite(fname,  cv2.cvtColor(rgb_8bit_data, cv2.COLOR_RGB2BGR ))
+    
+    
+    #### Save images ####
+    
+    if outpath.suffix == '.dng':
+        if options['verbose'] >=1 :
+            print('Saving output to {}'.format(outpath.with_suffix('.dng').as_posix()))
+        ref_img_path = glob.glob(os.path.join(args.impath, '*.dng'))[0]
+        save_as_dng(handheld_output, ref_img_path, outpath)
         
-                    'sharpening' : {'radius': args.radius,
-                                    'ammount': args.amount}
-                    }
-
-
-handheld_output = process(args.impath, options, params)
-handheld_output = np.nan_to_num(handheld_output)
-handheld_output = np.clip(handheld_output, 0, 1)
-
-
-# define a faster imsave for large png images
-def imsave(fname, rgb_8bit_data):
-    return cv2.imwrite(fname,  cv2.cvtColor(rgb_8bit_data, cv2.COLOR_RGB2BGR ))
-
-
-#### Save images ####
-imsave(args.outpath, img_as_ubyte(handheld_output))
-
-# Robustness map
-print('done')
+    else:
+        imsave(args.outpath, img_as_ubyte(handheld_output))
